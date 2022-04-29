28049f4e3118d0aa22a0a9db58ebacc8a5c4e29b
Rafael-Alencar <rafael.13.alencar13@gmail.com>
2022-04-29 14:43:45 -0400

    requisito 7

--- script.js
+++ script.js
@@ -20,3 +20,39 @@ window.onload = function() {
     }
     corInicial();
 }
+/*Seleciona as cores a ser utilizada*/
+function escolherCores (){
+    const buttonBlack = document.getElementsByClassName("color")[0]
+    const buttonGreen = document.getElementsByClassName("color")[1]
+    const buttonOrange = document.getElementsByClassName("color")[2]
+    const buttonBlue = document.getElementsByClassName("color")[3]
+
+    buttonBlack.addEventListener('click', function(){
+        buttonGreen.classList.remove('selected');
+        buttonOrange.classList.remove('selected');
+        buttonBlue.classList.remove('selected');
+        buttonBlack.classList.add('selected');
+
+    })
+    buttonGreen.addEventListener('click', function(){
+        buttonBlack.classList.remove('selected');
+        buttonOrange.classList.remove('selected');
+        buttonBlue.classList.remove('selected');
+        buttonGreen.classList.add('selected');
+
+    })
+    buttonOrange.addEventListener('click', function(){
+        buttonGreen.classList.remove('selected');
+        buttonBlack.classList.remove('selected');
+        buttonBlue.classList.remove('selected');
+        buttonOrange.classList.add('selected');
+
+    })
+    buttonBlue.addEventListener('click', function(){
+        buttonGreen.classList.remove('selected');
+        buttonOrange.classList.remove('selected');
+        buttonBlack.classList.remove('selected');
+        buttonBlue.classList.add('selected');
+    })
+}
+escolherCores();
