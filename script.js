f0a7353cd4d5d938397f8f1866a9db1e2257c7bd
Rafael-Alencar <rafael.13.alencar13@gmail.com>
2022-05-01 09:49:40 -0400

    requisito 8

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 /* Criando uma div para criar os blocos.
  para colocar os blocos um abaixo do outro, fiz isso utilizando o CSS.*/
 function divQuadrados() {
@@ -14,45 +13,52 @@ divQuadrados();
 
 let colorSeleted = document.querySelector('.color');
 
-window.onload = function() {
+window.onload = function () {
     function corInicial() {
         colorSeleted.classList.add('selected');
     }
     corInicial();
 }
 /*Seleciona as cores a ser utilizada*/
-function escolherCores (){
+function escolherCores() {
     const buttonBlack = document.getElementsByClassName("color")[0]
     const buttonGreen = document.getElementsByClassName("color")[1]
     const buttonOrange = document.getElementsByClassName("color")[2]
     const buttonBlue = document.getElementsByClassName("color")[3]
 
-    buttonBlack.addEventListener('click', function(){
+    buttonBlack.addEventListener('click', function () {
         buttonGreen.classList.remove('selected');
         buttonOrange.classList.remove('selected');
         buttonBlue.classList.remove('selected');
         buttonBlack.classList.add('selected');
 
     })
-    buttonGreen.addEventListener('click', function(){
+    buttonGreen.addEventListener('click', function () {
         buttonBlack.classList.remove('selected');
         buttonOrange.classList.remove('selected');
         buttonBlue.classList.remove('selected');
         buttonGreen.classList.add('selected');
 
     })
-    buttonOrange.addEventListener('click', function(){
+    buttonOrange.addEventListener('click', function () {
         buttonGreen.classList.remove('selected');
         buttonBlack.classList.remove('selected');
         buttonBlue.classList.remove('selected');
         buttonOrange.classList.add('selected');
 
     })
-    buttonBlue.addEventListener('click', function(){
+    buttonBlue.addEventListener('click', function () {
         buttonGreen.classList.remove('selected');
         buttonOrange.classList.remove('selected');
         buttonBlack.classList.remove('selected');
         buttonBlue.classList.add('selected');
     })
+    const pintar = document.getElementById('pixel-board');
+    pintar.addEventListener('click', function(event) {
+      const selectedColor = document.querySelector('.selected');
+      event.target.style.backgroundColor = window.getComputedStyle(selectedColor).backgroundColor;
+    }); 
 }
 escolherCores();
+
+
